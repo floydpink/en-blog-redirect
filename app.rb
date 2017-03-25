f4e13367b42d4253ddf1e4038f22c0db73df83bf
Floyd Pink <imbleedingme@googlemail.com>
2017-03-25 12:11:57 -0400

    updates dependencies and the root url

--- app.rb
+++ app.rb
@@ -1,47 +1,47 @@
 require 'bundler/setup'
 require 'sinatra'
 
-ROOT = "http://www.harimenon.com"
+ROOT = "https://new.harimenon.com"
 
 # Redirect
 # http://en.blog.harimenon.com/2009/11/laziness-prevails.html
 # to
-# http://www.harimenon.com/blog/2009/11/01/laziness-prevails/
+# https://harimenon.com/2009/11/01/laziness-prevails/
 get '/2009/11/laziness-prevails.html' do
-  redirect "#{ROOT}/blog/2009/11/02/laziness-prevails/", 301
+  redirect "#{ROOT}/2009/11/02/laziness-prevails/", 301
 end
 get '/2009/09/husn-e-haqiqi-english-translation.html' do
-  redirect "#{ROOT}/blog/2009/09/20/husn-e-haqiqi-english-translation/", 301
+  redirect "#{ROOT}/2009/09/20/husn-e-haqiqi-english-translation/", 301
 end
 get '/2009/07/it-keeps-flowing.html' do
-  redirect "#{ROOT}/blog/2009/07/17/it-keeps-flowing/", 301
+  redirect "#{ROOT}/2009/07/17/it-keeps-flowing/", 301
 end
 get '/2009/06/out-of-college-and-not-yet-into-first.html' do
-  redirect "#{ROOT}/blog/2009/06/28/out-of-college-and-not-yet-into-first/", 301
+  redirect "#{ROOT}/2009/06/28/out-of-college-and-not-yet-into-first/", 301
 end
 get '/2009/06/why-is-it-that-i-am-not-able-to-blog.html' do
-  redirect "#{ROOT}/blog/2009/06/23/why-is-it-that-i-am-not-able-to-blog/", 301
+  redirect "#{ROOT}/2009/06/23/why-is-it-that-i-am-not-able-to-blog/", 301
 end
 get '/2009/03/if.html' do
-  redirect "#{ROOT}/blog/2009/03/13/if/", 301
+  redirect "#{ROOT}/2009/03/13/if/", 301
 end
 get '/2009/03/to-this-i-put-my-name.html' do
-  redirect "#{ROOT}/blog/2009/03/13/to-this-i-put-my-name/", 301
+  redirect "#{ROOT}/2009/03/13/to-this-i-put-my-name/", 301
 end
 get '/2008/06/13-year-old-addressing-un.html' do
-  redirect "#{ROOT}/blog/2008/06/07/13-year-old-addressing-un/", 301
+  redirect "#{ROOT}/2008/06/07/13-year-old-addressing-un/", 301
 end
 get '/2007/12/noir-verses.html' do
-  redirect "#{ROOT}/blog/2007/12/05/noir-verses/", 301
+  redirect "#{ROOT}/2007/12/05/noir-verses/", 301
 end
 get '/2005/09/my-first-ever-blog.html' do
-  redirect "#{ROOT}/blog/2005/09/18/my-first-ever-blog/", 301
+  redirect "#{ROOT}/2005/09/18/my-first-ever-blog/", 301
 end
 
 # Redirect
 # http://en.blog.harimenon.com/search/label/cyber%20AWOL
 # to
-# http://www.harimenon.com/blog/categories/cyber-awol/
+# https://harimenon.com/categories/cyber-awol/
 
 # DevTool -> devtools
 # asp.net -> asp-net
@@ -49,20 +49,20 @@ end
 
 get '/search/label/:label' do |label|
   category = label.downcase.gsub(/\W/) { '-' }
-  redirect "#{ROOT}/blog/categories/#{category}/", 301
+  redirect "#{ROOT}/categories/#{category}/", 301
 end
 
 # http://en.blog.harimenon.com/2011_12_01_archive.html
 # =>
-# http://www.harimenon.com/blog/archives/
+# https://harimenon.com/archives/
 get '/:year_:month_:day_archive.html' do
-  redirect "#{ROOT}/blog/archives/", 301
+  redirect "#{ROOT}/archives/", 301
 end
 
 
-# en.blog.harimenon.com -> www.harimenon.com/blog/categories/english-blog/
+# en.blog.harimenon.com -> www.harimenon.com/categories/english-blog/
 get '/' do
-  redirect "#{ROOT}/blog/categories/english-blog/", 301
+  redirect "#{ROOT}/categories/english-blog/", 301
 end
 
 
