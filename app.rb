bc78e18caf2b153e772fb009980c6f67bb5bfd79
Floyd Pink <imbleedingme@googlemail.com>
2017-03-25 12:22:23 -0400

    updates some of the URL mappings to move into hexo

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 require 'sinatra'
 
-ROOT = "https://new.harimenon.com"
+ROOT = "https://harimenon.com"
 
 # Redirect
 # http://en.blog.harimenon.com/2009/11/laziness-prevails.html
@@ -11,7 +11,7 @@ get '/2009/11/laziness-prevails.html' do
   redirect "#{ROOT}/2009/11/02/laziness-prevails/", 301
 end
 get '/2009/09/husn-e-haqiqi-english-translation.html' do
-  redirect "#{ROOT}/2009/09/20/husn-e-haqiqi-english-translation/", 301
+  redirect "#{ROOT}/2009/09/21/husn-e-haqiqi-english-translation/", 301
 end
 get '/2009/07/it-keeps-flowing.html' do
   redirect "#{ROOT}/2009/07/17/it-keeps-flowing/", 301
@@ -20,7 +20,7 @@ get '/2009/06/out-of-college-and-not-yet-into-first.html' do
   redirect "#{ROOT}/2009/06/28/out-of-college-and-not-yet-into-first/", 301
 end
 get '/2009/06/why-is-it-that-i-am-not-able-to-blog.html' do
-  redirect "#{ROOT}/2009/06/23/why-is-it-that-i-am-not-able-to-blog/", 301
+  redirect "#{ROOT}/2009/06/24/why-is-it-that-i-am-not-able-to-blog/", 301
 end
 get '/2009/03/if.html' do
   redirect "#{ROOT}/2009/03/13/if/", 301
@@ -29,7 +29,7 @@ get '/2009/03/to-this-i-put-my-name.html' do
   redirect "#{ROOT}/2009/03/13/to-this-i-put-my-name/", 301
 end
 get '/2008/06/13-year-old-addressing-un.html' do
-  redirect "#{ROOT}/2008/06/07/13-year-old-addressing-un/", 301
+  redirect "#{ROOT}/2008/06/08/13-year-old-addressing-un/", 301
 end
 get '/2007/12/noir-verses.html' do
   redirect "#{ROOT}/2007/12/05/noir-verses/", 301
@@ -60,9 +60,9 @@ get '/:year_:month_:day_archive.html' do
 end
 
 
-# en.blog.harimenon.com -> www.harimenon.com/categories/english-blog/
+# en.blog.harimenon.com -> www.harimenon.com/tags/english-blog/
 get '/' do
-  redirect "#{ROOT}/categories/english-blog/", 301
+  redirect "#{ROOT}/tags/english-blog/", 301
 end
 
 
